fba2058c60448a244f8f852a28f458e7955c1a7c
ruparee <eruparee@me.com>
2018-06-30 22:31:11 +0100

    module 9 work in progress

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,8 @@
+// This file isn't transpiled, so must use CommonJS and ES5
+
+// Register babel to trnspile before our tests run.
+require('babel-register')();
+
+// Disable webpack features that Mocha doesn't understand
+require.extensions['.css'] = function() {};
+
